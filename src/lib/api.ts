736b6f1e4c93624572d28f6c96b18af69fdfbc29
unknown <mukulgautam18112000@gmail.com>
2026-03-19 21:43:54 +0530

    Fixed issues

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { auth } from './firebase';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3005/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://inventories-api.elyfast.com',
   headers: {
     'Content-Type': 'application/json',
   },
